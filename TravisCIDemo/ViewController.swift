45c70afdd58b444e9e0ab1b0c0fc1967f9d5d2e8
yanglei <yanglei@basefex.com>
2019-11-12 19:46:59 +0800

    project init

---
+++ TravisCIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCIDemo
+//
+//  Created by yanglei on 2019/11/12.
+//  Copyright © 2019 yanglei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
