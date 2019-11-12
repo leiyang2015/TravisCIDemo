1b37f11490f6195d20f0f12ebcf4d14a41484501
yanglei <yanglei@basefex.com>
2019-11-12 20:33:29 +0800

    test

--- TravisCIDemoTests/TravisCIDemoTests.swift
+++ TravisCIDemoTests/TravisCIDemoTests.swift
@@ -12,6 +12,10 @@ import XCTest
 class TravisCIDemoTests: XCTestCase {
 
     override func setUp() {
+        
+        
+        
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
